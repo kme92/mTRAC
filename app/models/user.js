eeb3c681e177893a0837b93443db12447e74744a
Kevin Me <chekmanirov@gmail.com>
2015-01-21 00:42:33 -0500

    user model

---
+++ app/models/user.js
@@ -0,0 +1,23 @@
+// app/models/user.js
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+    local            : {
+        name         : String,
+        password     : String
+    }
+
+});
+
+// generate hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
